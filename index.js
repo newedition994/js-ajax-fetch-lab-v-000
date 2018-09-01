89a6b344dd926b443724bb0ced3228b0eb75c59d
Breyanna Vass <breyanna.bvass@gmail.com>
2018-09-01 23:25:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
 function getIssues() {
+  const repo = 'javascript-fetch-lab/issues';
+  fetch(repo, {
+    method: 'post',
+    headers: {
+      Authorization: `token ${getToken}`
+    }
+  })
 }
 
 function showIssues(json) {
+  document.getElementById('body').innerHTML = json;
 }
 
 function createIssue() {
+  const repo = '/javascript-fetch-lab/issues/';
+  fetch(repo, {
+    method: 'post',
+    body: 'test body',
+    headers: {
+      Authorization: `token ${getToken}`
+    }
+  }).then(res => res.json()).then(json => showIssues(json))
 }
 
 function showResults(json) {
 }
 
 function forkRepo() {
-  const repo = 'learn-co-curriculum/javascript-fetch-lab'
+  const repo = 'api.github.com/repos/learn-co-curriculum/javascript-fetch-lab/';
   //use fetch to fork it!
+  fetch(repo, {
+    method: 'post',
+    headers: {
+      Authorization: `token ${getToken}`
+    }
+  })
 }
 
 function getToken() {
